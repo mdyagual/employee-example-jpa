b4ac27173447be36457fa76f659dc1ede5cc9fbc
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 20:12:33 -0500

    Initial commit

---
+++ src/main/java/com/sofka/employeejavaexercise/EmployeeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.javatechnolessons.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
