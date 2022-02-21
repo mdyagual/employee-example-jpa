4309254deab74fdb4d624a2b200c8c6594168360
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 23:29:35 -0500

    Implementación del controlador para role

--- src/main/java/com/sofka/employeejavaexercise/controllers/RoleController.java
+++ src/main/java/com/sofka/employeejavaexercise/controllers/RoleController.java
@@ -1,9 +1,14 @@
 package com.sofka.employeejavaexercise.controllers;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import com.sofka.employeejavaexercise.models.Role;
 import com.sofka.employeejavaexercise.services.RoleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +23,57 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleController {
     @Autowired
     RoleService rService;
-    
+    //------------------------------------------GET--------------------------------------------
+    @GetMapping("/all")
+    //http://127.0.0.1:8081/role/all
+    public ArrayList<Role> obtenerProyectos(){
+        return this.rService.getRoles();
+    }
+
+    @GetMapping(path = "/{id}")
+    //http://127.0.0.1:8081/role/id
+    public Optional<Role> obtenerProyectoPorId(@PathVariable("id") Long id){
+        return this.rService.getRoleById(id);
+    }
+
+    //------------------------------------------POST--------------------------------------------
+    @PostMapping("/registrar")
+    //http://127.0.0.1:8081/role/registrar 
+    public Role guardarProyecto(@RequestBody Role p){      
+        return this.rService.saveRole(p);            
+    }
+
+    //------------------------------------------PUT--------------------------------------------
+    @PutMapping(path = "/actualizar")
+    //http://127.0.0.1:8081/role/actualizar  
+    public String actualizarProyecto(@RequestBody Role p){
+        boolean ok = this.rService.updateRole(p);
+        if(ok){
+            return "Usuario actualizado";
+        }else{
+           
+            return "No se pudo actualizar";
+        }        
+
+    }
+
+    //----------------------------------------DELETE--------------------------------------------
+    @DeleteMapping(path= "/eliminar/{id}")
+    //http://127.0.0.1:8081/role/eliminar/id
+    public String eliminarPorId(@PathVariable("id") Long id){        
+        boolean ok = this.rService.deleteRole(id);
+        if(ok){
+            return "Usuario eliminado";
+        }else{
+           
+            return "No se pudo eliminar";
+        }
+    }
+
+    @DeleteMapping("/eliminarTodo")
+    //http://127.0.0.1:8081/Role/eliminarTodo
+	public ResponseEntity<HttpStatus> eliminarTodosLosRoles() {
+		return this.rService.deleteAllRoles();
+
+	}
 }
