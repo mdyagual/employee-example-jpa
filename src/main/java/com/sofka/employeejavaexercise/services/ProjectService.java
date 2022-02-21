9b9130ad9ec44ec8bdc8255434c7e9461445f7ab
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 23:25:13 -0500

    Actualización del service para eliminar todo

--- src/main/java/com/sofka/employeejavaexercise/services/ProjectService.java
+++ src/main/java/com/sofka/employeejavaexercise/services/ProjectService.java
@@ -1,6 +1,8 @@
 package com.sofka.employeejavaexercise.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -65,5 +67,16 @@ public class ProjectService {
             return false;
         }
     }
+
+    //Eliminar todos los proyectos
+    public ResponseEntity<HttpStatus> deleteAllProjects() {
+		try {
+			pRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+
+	}
 
 }
