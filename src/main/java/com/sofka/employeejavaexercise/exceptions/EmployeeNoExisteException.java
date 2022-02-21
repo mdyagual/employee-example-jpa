0bba74c4482fe4b5d84294c28d9e0e413a567c83
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 22:47:39 -0500

    Implementación de los servicios

---
+++ src/main/java/com/sofka/employeejavaexercise/exceptions/EmployeeNoExisteException.java
@@ -0,0 +1,7 @@
+package com.sofka.employeejavaexercise.exceptions;
+
+public class EmployeeNoExisteException extends Exception {
+    public EmployeeNoExisteException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/sofka/employeejavaexercise/services/EmployeeService.java
+++ src/main/java/com/sofka/employeejavaexercise/services/EmployeeService.java
@@ -3,6 +3,10 @@ package com.sofka.employeejavaexercise.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.sofka.employeejavaexercise.exceptions.EmployeeNoExisteException;
 import com.sofka.employeejavaexercise.models.Employee;
 import com.sofka.employeejavaexercise.repositories.EmployeeRepository;
 
@@ -10,4 +14,58 @@ import com.sofka.employeejavaexercise.repositories.EmployeeRepository;
 public class EmployeeService {
     @Autowired
     EmployeeRepository eRepository;
+
+    //Obtener todos los empleados
+    public ArrayList<Employee> getEmployees(){
+        return (ArrayList<Employee>) eRepository.findAll();
+    }
+
+    //Obtener empleado por id
+    public Optional<Employee> getEmployeeById(Long id){
+        return eRepository.findById(id);
+    }
+
+    //Crear empleado
+    public Employee saveEmployee(Employee e){
+        return eRepository.saveAndFlush(e);
+    }
+
+    //Actualizar empleado
+    public boolean updateEmployee(Employee e){
+        Optional<Employee> op_e = getEmployeeById(e.getId());  
+        try{                       
+            if(op_e.isPresent()){                
+                eRepository.saveAndFlush(e);
+                return true;
+            }else{
+                throw new EmployeeNoExisteException("Usuario con id: "+e.getId()+" no existe");
+            }
+            
+        }catch(EmployeeNoExisteException emp){   
+            System.out.println(emp);         
+            return false;
+        }
+        
+    }
+
+    //Eliminar empleado
+    public boolean deleteEmployee(Long id){    
+        try{
+            Optional<Employee> op_e = getEmployeeById(id);             
+            if(op_e.isPresent()){
+                eRepository.deleteById(id);
+                return true;
+
+            }else{
+                throw new EmployeeNoExisteException("Usuario con id: "+id+" no existe");
+            }
+            
+        }catch(EmployeeNoExisteException e){   
+            System.out.println(e);         
+            return false;
+        }
+    }
+
+    
+
 }
