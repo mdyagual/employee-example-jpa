e42fc218b725d50ae127c02620baee1e44408d3e
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 22:55:44 -0500

    Excepción para manejo de la existencia de project

---
+++ src/main/java/com/sofka/employeejavaexercise/exceptions/ProjectNoExisteException.java
@@ -0,0 +1,8 @@
+package com.sofka.employeejavaexercise.exceptions;
+
+public class ProjectNoExisteException extends Exception{
+    public ProjectNoExisteException(String message){
+        super(message);
+    }
+    
+}
