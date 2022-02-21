adf70d72d1ca61f3a0b6197d6423d4d5c3421c1c
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 23:24:52 -0500

    Implementación de controlador para project

--- src/main/java/com/sofka/employeejavaexercise/controllers/ProjectController.java
+++ src/main/java/com/sofka/employeejavaexercise/controllers/ProjectController.java
@@ -1,9 +1,14 @@
 package com.sofka.employeejavaexercise.controllers;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import com.sofka.employeejavaexercise.models.Project;
 import com.sofka.employeejavaexercise.services.ProjectService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +23,60 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController {
     @Autowired
     ProjectService pService;
+
+    //------------------------------------------GET--------------------------------------------
+    @GetMapping("/all")
+    //http://127.0.0.1:8081/project/all
+    public ArrayList<Project> obtenerProyectos(){
+        return this.pService.getProjects();
+    }
+
+    @GetMapping(path = "/{id}")
+    //http://127.0.0.1:8081/project/id
+    public Optional<Project> obtenerProyectoPorId(@PathVariable("id") Long id){
+        return this.pService.getProjectById(id);
+    }
+
+    //------------------------------------------POST--------------------------------------------
+    @PostMapping("/registrar")
+    //http://127.0.0.1:8081/project/registrar 
+    public Project guardarProyecto(@RequestBody Project p){      
+        return this.pService.saveProject(p);            
+    }
+
+    //------------------------------------------PUT--------------------------------------------
+    @PutMapping(path = "/actualizar")
+    //http://127.0.0.1:8081/Project/actualizar  
+    public String actualizarProyecto(@RequestBody Project p){
+        boolean ok = this.pService.updateProject(p);
+        if(ok){
+            return "Usuario actualizado";
+        }else{
+           
+            return "No se pudo actualizar";
+        }        
+
+    }
+
+    //----------------------------------------DELETE--------------------------------------------
+    @DeleteMapping(path= "/eliminar/{id}")
+    //http://127.0.0.1:8081/Project/eliminar/id
+    public String eliminarPorId(@PathVariable("id") Long id){        
+        boolean ok = this.pService.deleteProject(id);
+        if(ok){
+            return "Usuario eliminado";
+        }else{
+           
+            return "No se pudo eliminar";
+        }
+    }
+
+    @DeleteMapping("/eliminarTodo")
+    //http://127.0.0.1:8081/Project/eliminarTodo
+	public ResponseEntity<HttpStatus> eliminarTodosLosProyectos() {
+		return this.pService.deleteAllProjects();
+
+	}
+
 }
+
