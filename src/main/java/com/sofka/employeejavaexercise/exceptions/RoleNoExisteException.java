7093b7f1b62aa131143cf85b02bb2077ab8f0e9f
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-20 23:02:30 -0500

    Excepción para el control de existencia de Role

---
+++ src/main/java/com/sofka/employeejavaexercise/exceptions/RoleNoExisteException.java
@@ -0,0 +1,9 @@
+
+package com.sofka.employeejavaexercise.exceptions;
+
+public class RoleNoExisteException extends Exception{
+    public RoleNoExisteException(String message){
+        super(message);
+    }
+    
+}
